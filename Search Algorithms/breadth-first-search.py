70daea831df695293fdb076951bf3c68d2e3151d
Morvin Ian <morvinian@gmail.com>
2022-01-29 00:48:35 +0300

    Update breadth-first-search.py

--- Search Algorithms/breadth-first-search.py
+++ Search Algorithms/breadth-first-search.py
@@ -1,4 +1,4 @@
 # Its a trasversing algo that starts from a certain source nodelayerwise and
-# explores its neighbouring nodes. 
+# explores its neighbouring nodes. Work under the FIFO principle
 
 #Todo
