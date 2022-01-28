f402bec897e9e361493741c558fd0f20b24a04d8
“Morvin-Ian” <morvinian@gmail.com>
2022-01-29 00:36:05 +0300

    additions

---
+++ Search Algorithms/breadth-first-search.py
@@ -0,0 +1,4 @@
+# Its a trasversing algo that starts from a certain source nodelayerwise and
+# explores its neighbouring nodes. 
+
+#Todo
---
+++ Search Algorithms/depth-first-search.py
@@ -0,0 +1,4 @@
+# The search starts at the root node and explores it
+# as far as possible along each branch before backtracking
+
+#Todo
