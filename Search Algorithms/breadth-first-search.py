7c335ea7b8b9717a20eeb145d5cf3909dfdf2aa6
“Morvin-Ian” <morvinian@gmail.com>
2022-01-31 11:07:49 +0300

    additions

--- Search Algorithms/breadth-first-search.py
+++ Search Algorithms/breadth-first-search.py
@@ -1,4 +1,55 @@
 # Its a trasversing algo that starts from a certain source nodelayerwise and
-# explores its neighbouring nodes. Work under the FIFO principle
+# explores its neighbouring nodes. 
 
-#Todo
+
+from collections import defaultdict
+class Graph:
+
+	# Constructor
+	def __init__(self):
+		self.graph = defaultdict(list)
+
+	def addEdge(self,u,v):
+		self.graph[u].append(v)
+
+	# Function to print a BFS of graph
+	def BFS(self, s):
+
+		# Mark all the vertices as not visited
+		visited = [False] * (max(self.graph) + 1)
+
+		# Create a queue for BFS
+		queue = []
+
+		queue.append(s)
+		visited[s] = True
+
+		while queue:
+
+			# Dequeue a vertex from
+			# queue and print it
+			s = queue.pop(0)
+			print (s, end = " ")
+
+			# Get all adjacent vertices of the
+			# dequeued vertex s. If a adjacent
+			# has not been visited, then mark it
+			# visited and enqueue it
+			for i in self.graph[s]:
+				if visited[i] == False:
+					queue.append(i)
+					visited[i] = True
+
+
+g = Graph()
+g.addEdge(0, 1)
+g.addEdge(0, 2)
+g.addEdge(1, 2)
+g.addEdge(2, 0)
+g.addEdge(2, 3)
+g.addEdge(3, 3)
+
+print ("Breadth First Traversal (starting from vertex 2)")
+g.BFS(2)
+
+# This code is contributed by Neelam Yadav
--- Search Algorithms/depth-first-search.py
+++ Search Algorithms/depth-first-search.py
@@ -1,4 +1,77 @@
 # The search starts at the root node and explores it
 # as far as possible along each branch before backtracking
 
-#Todo
+
+from collections import defaultdict
+
+class Graph:
+	def __init__(self):
+		# default dictionary to store graph
+		self.graph = defaultdict(list)
+
+	# function to add an edge to graph
+	def addEdge(self, u, v):
+		self.graph[u].append(v)
+
+	# A function used by DFS
+	def DFSUtil(self, v, visited):
+
+		# Mark the current node as visited
+		# and print it
+		visited.add(v)
+		print(v, end=' ')
+
+		# Recur for all the vertices
+		# adjacent to this vertex
+		for neighbour in self.graph[v]:
+			if neighbour not in visited:
+				self.DFSUtil(neighbour, visited)
+
+
+	# recursive DFSUtil()
+	def DFS(self, v):
+		visited = set()
+
+		# Call the recursive helper function
+		self.DFSUtil(v, visited)
+
+# Driver code
+
+
+# Create a graph given
+# in the above diagram
+g = Graph()
+g.addEdge(0, 1)
+g.addEdge(0, 2)
+g.addEdge(1, 2)
+g.addEdge(2, 0)
+g.addEdge(2, 3)
+g.addEdge(3, 3)
+
+print("Following is DFS from (starting from vertex 2)")
+g.DFS(2)
+
+
+# Shorter
+graph = {
+  '5' : ['3','7'],
+  '3' : ['2', '4'],
+  '7' : ['8'],
+  '2' : [],
+  '4' : ['8'],
+  '8' : []
+}
+
+visited = set() # Set to keep track of visited nodes of graph.
+
+def dfs(visited, graph, node):  #function for dfs 
+    if node not in visited:
+        print (node)
+        visited.add(node)
+        for neighbour in graph[node]:
+            dfs(visited, graph, neighbour)
+
+# Driver Code
+print("Following is the Depth-First Search")
+dfs(visited, graph, '5')
+
